69c543fc06db15fd930460a8a6ebde6665a978b6
Petrik2 <peter.kisel2@gmail.com>
2020-11-29 21:34:58 +0100

    úvod spravený ...yeeeessss madafakaaaa

---
+++ JS/main.js
@@ -0,0 +1,30 @@
+//slick
+$(document).ready(function(){
+    $('.slider').slick({
+      autoplay: true,
+      autoplaySpeed: 3000,
+      dots: true,
+      infinite: true,
+      fade: true,
+      speed: 1200,
+      adaptiveHeight: false,
+      focusOnSelect: true,
+      arrows: false,
+    });
+  });
+
+// hide/show header and footer script
+var header = $(".head-container"),
+    footer = $(".footer-container"),
+    i = null,
+    j = null;
+
+
+    $("body").mousemove(function() {
+      clearTimeout(i);
+      clearTimeout(j);
+      header.slideDown();
+      footer.show("slide", {direction: "down"});
+      i = setTimeout('header.slideUp();', 1500);
+      j = setTimeout('footer.hide("slide", {direction: "down"});', 1500);
+    });
