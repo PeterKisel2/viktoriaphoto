884f5e612b57bfd9a22611f7fe18236b08ee06bc
Petrik2 <peter.kisel2@gmail.com>
2021-01-23 15:40:07 +0100

    no message

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,7 @@
 //slick
 $(document).ready(function(){
     $('.slider').slick({
-      autoplay: true,
+      autoplay: false,
       autoplaySpeed: 3000,
       dots: true,
       infinite: true,
@@ -16,15 +16,63 @@ $(document).ready(function(){
 //media query - show menu on #bars click
 $(document).ready(function(){
   $("#bars").click(function (e) {
-    e.preventDefault()
-    if ($(".primary > .list-item").not("#bars").css("transform") === "matrix(0, 0, 0, 0, 0, 0)") {
-    $(".primary > .list-item").not("#bars").css("transform", "scale(1)");
+    var hiddenmenu = $(".primary > .list-item").not("#bars");
+    e.preventDefault();
+    if (hiddenmenu.css("transform") === "matrix(0, 0, 0, 0, 0, 0)") {
+      hiddenmenu.css("transform", "scale(1)");
     }
-    else { $(".primary > .list-item").not("#bars").css("transform", "scale(0)");
+    else { 
+      hiddenmenu.css("transform", "scale(0)");
     }
   });
 });
 
+//lightbox
+
+$(document).ready(function(){
+  var overlay = $("#overlay")
+      img_container = $(".img-container")
+
+  $(".img-container").hide();
+  $(".pictures-thumb").find("a").click(function (e) {
+
+      var href = $(this).attr("href");
+          image = $("<img>", { src: href});
+      
+      $(".img-container").html( image )
+
+      overlay.slideToggle({
+        duration: 250,
+        complete: function(){$(".img-container").fadeToggle({
+          duration: 250
+        });}
+      });
+
+      e.preventDefault();
+  });
+
+  //hide overlay on click
+  overlay.click(function (e) {
+    $(".img-container").fadeToggle({
+        duration: 250,
+        complete: function(){overlay.slideToggle({
+          duration: 250
+        });}
+    }); 
+  });
+
+  //hide overlay on esc key
+  $(document).keyup(function (e) { 
+    if (e.which === 27 && overlay.css("display") === "block")  {
+      $(".img-container").fadeToggle({
+        duration: 250,
+        complete: function(){overlay.slideToggle({
+          duration: 250
+        });}
+      });
+    };
+  });
+});
 
 //bottom-left page info
 $(document).ready(function(){
